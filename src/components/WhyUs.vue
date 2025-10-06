d1e34127df82e0e9611ecbbbf4249f02045674df
Randi-95 <randiganteng9528@gmail.com>
2025-10-06 19:17:25 +0700

    (update) mengapa roti gokki

--- src/components/WhyUs.vue
+++ src/components/WhyUs.vue
@@ -1,7 +1,17 @@
+<script setup lang="ts">
+import { Icon } from '@iconify/vue';
+
+</script>
+
 <template>
     <div class="w-full h-screen flex">
         <div class="">
-           <!-- Konten Disini -->
+           <p class="teks">Randi</p>
+            <Icon icon="mdi:fire" class="text-primary w-6 h-6" />
         </div>
     </div>
 </template>
+
+<style scoped>
+
+</style>
