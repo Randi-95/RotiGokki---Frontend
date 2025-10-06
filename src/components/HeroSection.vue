cbb14d46e186450c267a50ba8e431704454d5a9f
Randi-95 <randiganteng9528@gmail.com>
2025-10-06 11:48:21 +0700

    (update) hero section

--- src/components/HeroSection.vue
+++ src/components/HeroSection.vue
@@ -2,7 +2,7 @@
     <div class="w-full lg:h-screen flex items-center">
         <div class="flex flex-col lg:flex-row px-3 pt-26 lg:pt-0 lg:px-25">
             <div class="lg:w-1/2 order-2 flex flex-col justify-center">
-                <h1 class="text-[#333333] font-fredokaone font-normal text-5xl">Kenikmatan Sejati, <span class="text-pink">Lembut</span> dan <span class="text-primary">Creamy</span> diSetiap Gigitan</h1>
+                <h1 class="text-[#333333] font-fredokaone font-normal text-3xl lg:text-5xl">Kenikmatan Sejati, <span class="text-pink">Lembut</span> dan <span class="text-primary">Creamy</span> diSetiap Gigitan</h1>
                 <p class="font-poppins mt-3 text-[#272727] ">Roti lembut dengan cream halus yang lumer di mulut. Dibuat penuh cinta untuk sarapan, camilan, atau hadiah manis.</p>
                 <div class="flex mt-4 gap-2">
                     <button class="bg-primary font-bold border border-transparent rounded-sm font-poppins text-white px-4 py-2 ">
