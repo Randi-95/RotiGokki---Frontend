baabbec3e177c3d2f7202d2b8bc4b975e77913b7
rizqiseptian <rizqiseptianramadan@gmail.com>
2025-10-07 13:30:06 +0700

    menambahkan section why pada website

--- src/components/WhyUs.vue
+++ src/components/WhyUs.vue
@@ -4,10 +4,46 @@ import { Icon } from '@iconify/vue';
 </script>
 
 <template>
-    <div class="w-full h-screen flex">
-        <div class="">
-           <p class="teks">Randi</p>
-            <Icon icon="mdi:fire" class="text-primary w-6 h-6" />
+    <div class="w-full lg:h-screen flex items-center">
+        <div class="w-full flex flex-col items-center px-4 py-10 mt-10 lg:flex-row width-1/2">
+            <div class="justify-center items-center flex-col w- h-397px">
+                <img src="/public/why.png" alt="">
+            </div>
+            <div class="flex flex-col w-1/2 h-screen">
+                <div class="py-1 px-3 mb-2 mt-1 justify-start self-start">
+                <p class="bg-[#F9D3DE] w-fit mb-2 py-1 px-9 rounded-sm text-[#F12967] font-poppins">Kenapa RotiGokki?</p>
+            </div>
+                <div class="flex flex-col items-center text-center mb-10 py-1 px-3 mt-1">
+                    <p class="text-primary font-normal text-4xl lg:text-5xl font-fredokaone text-left">Nikmati rasa terbaik roti lembut & creamy kami</p>
+                </div>
+                <div class="flex flex=row mb-10 py-1 px-3 mt-1">
+                    <div class="p-3 rounded-[18px] bg-[#F9D3DE] max-w-[64px] max-h-[54px]">
+                        <Icon icon="mdi:fire" class="text-[#F12967] w-[28px] h-[28px]"/>
+                    </div>
+                    <div class="flex flex-col px-3">
+                        <h1 class="font-fredokaone px-2 text-[20px]">Fresh dari oven setiap hari</h1>
+                        <p class="font-poppins text-[18] px-2">Setiap roti dipanggang segar setiap hari untuk menjamin kualitas terbaik.</p>
+                    </div>
+                </div>
+                <div class="flex flex=row mb-10 py-1 px-3 mt-1">
+                    <div class="p-3 rounded-[18px] bg-[#F9D3DE] max-w-[64px] max-h-[54px]">
+                        <Icon icon="mdi:fire" class="text-[#F12967] w-[28px] h-[28px]"/>
+                    </div>
+                    <div class="flex flex-col px-3">
+                        <h1 class="font-fredokaone px-2 text-[20px]">Fresh dari oven setiap hari</h1>
+                        <p class="font-poppins text-[18] px-2">Setiap roti dipanggang segar setiap hari untuk menjamin kualitas terbaik.</p>
+                    </div>
+                </div>
+                <div class="flex flex=row mb-10 py-1 px-3 mt-1">
+                    <div class="p-3 rounded-[18px] bg-[#F9D3DE] max-w-[64px] max-h-[54px]">
+                        <Icon icon="mdi:fire" class="text-[#F12967] w-[28px] h-[28px]"/>
+                    </div>
+                    <div class="flex flex-col px-3">
+                        <h1 class="font-fredokaone px-2 text-[20px]">Fresh dari oven setiap hari</h1>
+                        <p class="font-poppins text-[18] px-2">Setiap roti dipanggang segar setiap hari untuk menjamin kualitas terbaik.</p>
+                    </div>
+                </div>
+            </div>
         </div>
     </div>
 </template>
