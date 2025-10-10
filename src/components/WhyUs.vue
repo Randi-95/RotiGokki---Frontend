acb3d605c0df0e9dcf1f0fd289a1c4f9f30d516b
rizqiseptian <rizqiseptianramadan@gmail.com>
2025-10-10 12:22:26 +0700

    update

--- src/components/WhyUs.vue
+++ src/components/WhyUs.vue
@@ -5,15 +5,15 @@ import { Icon } from '@iconify/vue';
 
 <template>
     <div class="w-full lg:h-screen flex items-center">
-        <div class="w-full flex flex-col items-center  py-10 mt-10 lg:flex-row">
-            <div class="justify-center items-center flex-col w-2/3  flex">
+        <div class="w-full flex flex-col items-center mt-10 lg:flex-row sm:mb-0">
+            <div class="justify-center items-center flex-col w-2/3 flex lg:w-1/2  sm:order-0 md:order-0 lg:order-0 mb-10 lg:mb-0">
                 <img src="/public/why.png" alt="">
             </div>
-            <div class="flex flex-col w-1/2 h-screen items-center justify-center">
-                <div class="py-1 px-3 mb-2 mt-1 justify-start self-start">
-                    <p class="bg-[#F9D3DE] w-fit mb-1 py-1 px-9 rounded-sm text-pink font-medium font-poppins">Kenapa RotiGokki?</p>
+            <div class="flex flex-col lg:w-1/2 md:w-1/2 h-screen items-center justify-center  lg:pt-0 lg:px-25">
+                <div class="py-3 px-5 mb-2 mt-1 justify-start self-start sm:justify-center">
+                    <p class="bg-[#F9D3DE] w-fit mb-1 px-12 py-[7px] rounded-[15px] text-pink text-sm font-poppins md:order-2 sm:order-2">Kenapa RotiGokki?</p>
                 </div>
-                <div class="flex flex-col items-center text-center mb-2 py-1 px-3">
+                <div class="flex flex-col items-center text-center mb-2 py-1 px-3 m">
                     <p class="text-primary font-normal text-4xl lg:text-5xl font-fredokaone text-left">Nikmati rasa terbaik roti lembut & creamy kami.</p>
                 </div>
                 <div class="flex flex-row mb-4 py-1 px-3 items-center">
