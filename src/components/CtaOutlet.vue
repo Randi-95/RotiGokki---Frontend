712f75737f38b8ac499088869aeba4f050bf3021
Rereyys <w.horayys@gmail.com>
2025-10-12 21:42:27 +0700

    Position Fixed | CTA outlet

--- src/components/CtaOutlet.vue
+++ src/components/CtaOutlet.vue
@@ -1,13 +1,16 @@
 <template>
-    <div class="w-full min-h-screen flex flex-col-reverse lg:flex-row items-center px-6 lg:px-90 bg-white">
+    <div class="w-full min-h-screen flex flex-col-reverse lg:flex-row items-center px-6 lg:px-70 bg-white">
         <div class="left-container w-full lg:h-screen flex items-center">
-            <div class="">
-                <h1 class="font-fredokaone text-4xl font-bold text-primary mb-4">Pengalaman <br>Langsung dimulai <br><span class="text-pink text-5xl italic">di sini</span></h1>
-                <p class="text-[#000000] font-poppins">Kunjungi outlet terdekat untuk <span class="font-semibold">pengalaman</span> <br>dan <span class="font-semibold">produk terbaik</span></p>
-                <button class="bg-[#FEDBE6] font-bold rounded-sm font-poppins text-pink px-3 py-2 margin-top-20 mt-6">  Kunjungi Outlet!</button>
+            <div class="text-right mt-10">
+                <h1 class="font-fredokaone text-5xl font-bold text-primary mb-3">Pengalaman <br>Langsung dimulai <br><span class="text-pink text-6xl">di sini</span></h1>
+                <p class="text-[#000000] font-poppins text-2xl">Kunjungi outlet terdekat untuk <br><span class="font-semibold">pengalaman</span> dan <span class="font-semibold">produk terbaik</span></p>
+                <button class="bg-[#FEBDE6] font-bold rounded-sm font-poppins text-pink px-3 py-2 mt-6 hover:bg-pink-400 hover:text-white transform hover:scale-105 transition-all duration-300 ease-in-out hover:shadow-lg">
+  Kunjungi Outlet
+</button>
+
             </div>
         </div>
-        <div class="right-container w-full flex items-center">
+        <div class="right-container w-full flex items-center pt-mt-10 lg:mt-0 lg:h-screen">
             <div class="">
                 <img src="/public/outletcta.png" alt="">
             </div>
