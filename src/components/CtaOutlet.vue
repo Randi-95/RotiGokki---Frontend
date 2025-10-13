6c976b66d53821f9b3f3b064cbe7a1c11c8f6e7c
Randi-95 <randiganteng9528@gmail.com>
2025-10-13 07:05:46 +0700

    responsive CTA , Kemitraan Section

--- src/components/CtaOutlet.vue
+++ src/components/CtaOutlet.vue
@@ -2,9 +2,9 @@
     <div class="w-full min-h-screen flex flex-col-reverse lg:flex-row items-center px-6 lg:px-70 bg-white">
         <div class="left-container w-full lg:h-screen flex items-center">
             <div class="text-right mt-10">
-                <h1 class="font-fredokaone text-5xl font-bold text-primary mb-3">Pengalaman <br>Langsung dimulai <br><span class="text-pink text-6xl">di sini</span></h1>
-                <p class="text-[#000000] font-poppins text-2xl">Kunjungi outlet terdekat untuk <br><span class="font-semibold">pengalaman</span> dan <span class="font-semibold">produk terbaik</span></p>
-                <button class="bg-[#FEBDE6] font-bold rounded-sm font-poppins text-pink px-3 py-2 mt-6 hover:bg-pink-400 hover:text-white transform hover:scale-105 transition-all duration-300 ease-in-out hover:shadow-lg"> Kunjungi Outlet</button>
+                <h1 class="font-fredokaone text-4xl font-normal text-primary mb-3">Pengalaman Langsung dimulai <br class="hidden lg:block"><span class="text-pink">di sini</span></h1>
+                <p class="text-[#333333] font-poppins text-xl">Kunjungi outlet terdekat untuk <span class="font-semibold">pengalaman</span> dan <span class="font-semibold">produk terbaik</span></p>
+                <button class="bg-[#FEBDE6] font-bold rounded-sm font-poppins text-pink px-3 py-2 mt-6 hover:bg-pink-400 hover:text-white transform hover:scale-105 transition-all duration-300 ease-in-out hover:shadow-lg"> Kunjungi Outlet!</button>
 
             </div>
         </div>
--- src/components/Kemitraan.vue
+++ src/components/Kemitraan.vue
@@ -3,12 +3,14 @@ import { Icon } from '@iconify/vue';
 </script>
 
 <template>
-    <div class="w-full h-screen flex py-12 px-20"> <div class="w-1/2 flex justify-center items-center">
+    <div class="w-full flex flex-col lg:flex-row py-12 px-3 lg:px-20"> 
+        <div class="lg:w-1/2 flex justify-center items-center">
             <img src="/public/kemitraan.png" alt="Promo Kemitraan Roti Gokki" class="max-h-full object-contain">
         </div>
 
-        <div class="w-1/2 pl-16 pt-8"> <h2 class="text-black font-fredokaone text-4xl mb-12 leading-snug"> Bergabung Jadi Mitra Roti <span class="text-primary">Gokki</span>, <br>
-                <span class="text-pink">Nikmati Keuntungannya!</span>
+        <div class="lg:w-1/2 lg:pl-16 pt-8"> 
+            <h2 class="text-[#333333] font-fredokaone text-[30px] mb-12 leading-snug"> Bergabung Jadi Mitra Roti <span class="text-primary">Gokki</span>,<br class="hidden lg:block">Nikmati
+                <span class="text-pink">Keuntungannya!</span>
             </h2>
 
             <div class="space-y-6">
@@ -17,8 +19,8 @@ import { Icon } from '@iconify/vue';
                         <Icon icon ="solar:box-outline" class="w-7 h-7"></Icon>
                     </div>
                     <div>
-                        <h3 class="font-fredokaone text-black text-xl">Modal Awal 100 Roti Gokki Gratis</h3>
-                        <p class="font-poppins text-lg text-gray-700">Langsung siap jualan dan raih keuntungan pertama tanpa modal stok.</p>
+                        <h3 class="font-fredokaone text-[#333333] text-xl">Modal Awal 100 Roti Gokki Gratis</h3>
+                        <p class="font-poppins text-[16px] text-gray-700">Langsung siap jualan dan raih keuntungan pertama tanpa modal stok.</p>
                     </div>
                 </div>
 
@@ -27,8 +29,8 @@ import { Icon } from '@iconify/vue';
                         <Icon icon ="solar:shop-outline" class="w-7 h-7"></Icon>
                     </div>
                     <div>
-                        <h3 class="font-fredokaone text-black text-xl">Booth Stand Eksklusif & Profesional</h3>
-                        <p class="font-poppins text-lg text-gray-700">Tampil menarik dan bangun kepercayaan pelanggan seketika.</p>
+                        <h3 class="font-fredokaone text-[#333333] text-xl">Booth Stand Eksklusif & Profesional</h3>
+                        <p class="font-poppins text-[16px] text-gray-700">Tampil menarik dan bangun kepercayaan pelanggan seketika.</p>
                     </div>
                 </div>
 
@@ -37,8 +39,8 @@ import { Icon } from '@iconify/vue';
                         <Icon icon ="oui:line-chart" class="w-7 h-7"></Icon>
                     </div>
                     <div>
-                        <h3 class="font-fredokaone text-black text-xl">X-Banner untuk Promosi Efektif</h3>
-                        <p class="font-poppins text-lg text-gray-700">Alat marketing efektif untuk menarik lebih banyak pembeli.</p>
+                        <h3 class="font-fredokaone text-[#333333] text-xl">X-Banner untuk Promosi Efektif</h3>
+                        <p class="font-poppins text-[16px] text-gray-700">Alat marketing efektif untuk menarik lebih banyak pembeli.</p>
                     </div>
                 </div>
 
@@ -47,15 +49,15 @@ import { Icon } from '@iconify/vue';
                         <Icon icon ="solar:bag-2-outline" class="w-7 h-7"></Icon>
                     </div>
                     <div>
-                        <h3 class="font-fredokaone text-black text-xl">Seragam Kaos & Celemek (Apron) Eksklusif</h3>
-                        <p class="font-poppins text-lg text-gray-700">Tunjukkan identitas brand yang kuat dan profesional.</p>
+                        <h3 class="font-fredokaone text-[#333333] text-xl">Seragam Kaos & Celemek (Apron) Eksklusif</h3>
+                        <p class="font-poppins text-[16px] text-gray-700">Tunjukkan identitas brand yang kuat dan profesional.</p>
                     </div>
                 </div>
             </div>
 
-            <div class="mt-12">
+            <div class="mt-5">
                 <div class="flex justify-center">
-                    <button class="font-fredokaone text-white bg-pink hover:bg-pink-700 transition-colors py-4 px-10 rounded-xl text-xl w-auto">
+                    <button class="font-fredokaone text-white bg-pink hover:bg-pink-700 transition-colors py-3 px-10 btn-mitra text-xl w-auto">
                         Gabung Sekarang
                     </button>
                 </div>
@@ -63,3 +65,9 @@ import { Icon } from '@iconify/vue';
         </div>
     </div>
 </template>
+
+<style scoped>
+    .btn-mitra{
+        border-radius: 10px 29px 10px 29px;
+    }
+</style>
