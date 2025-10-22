2c9972a1097383ac44d533035e74cb013ea86022
Randi-95 <randiganteng9528@gmail.com>
2025-10-22 07:02:07 +0700

    (update) login

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -4,25 +4,13 @@ import { Icon } from '@iconify/vue';
 
 
 <template>
-  <div class="h-screen w-screen grid md:grid-cols-[1fr_max-content_1fr] grid-c">
-        
-        <div class="hidden md:block bg-primary relative overflow-hidden">
-             <div class="absolute bottom-0 right-0 z-10 transform translate-x-1/2 translate-y-1/2">
-                <img src="/public/login.png" alt="" class="w-40 h-40">
-                    </img>
+    <div class="h-screen w-screen bg-primary relative">
+        <div class="lg:w-[70%] h-full mx-auto bg-white px-10 flex flex-col justify-center">
+            <div class="flex justify-center">
+                <img src="/public/Logo.png" alt="" class="h-40 w-40">
             </div>
-        </div>
-
-        <div class="flex items-center justify-center p-4">
-            <div class="w-full max-w-md mx-auto p-8 md:p-12 bg-white rounded-xl shadow-2xl z-20">
-                
-                <div class="flex justify-center">
-                    <img src="/public/Logo.png" alt="" class="h-40 w-40">
-                </div>
-
-                <h2 class="text-2xl font-semibold text-gokki-blue text-center mb-8">Login</h2>
-                
-                <form>
+                <h2 class="text-4xl font-bold text-primary font-poppins text-gokki-blue text-center mb-8">Login</h2>
+            <form>
                     <div class="mb-6">
                         <div class="relative">
                             <span class="absolute inset-y-0 left-0 flex items-center pl-3 text-gray-400">
@@ -63,12 +51,9 @@ import { Icon } from '@iconify/vue';
                         </button>
                     </div>
                 </form>
-            </div>
         </div>
 
-        <div class="hidden md:block bg-primary">
-            </div>
-
+        <img src="/public/maskot_login.png" alt="" class="absolute lg:block w-70 bottom-0 hidden">
     </div>
 </template>
 
